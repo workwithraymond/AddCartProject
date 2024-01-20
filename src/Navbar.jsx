f634343114fd1837b57fd6a30f28848d72e0ee8e
workwithraymond <workwithraymond01@gmail.com>
2024-01-19 20:54:33 -0500

    Last commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className='nav-center'>
-        <h4>useReducer</h4>
+        <h4>Cell Phone Store</h4>
         <div className='nav-container'>
           <FaCartPlus className='cart-icon' />
           <div className='amount-container'>
